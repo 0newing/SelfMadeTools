06f14b696cabbac7b6f1a6d25e2a6c9ed0254312
阿金 <disciple_sneaker@163.com>
2019-06-24 22:45:29 +0800

    # 6.24更新 - 删除冗余文件 - 增加文件工具功能     - 获取文件特征码(SHA1,MD5,SHA256)

--- src/io/github/curatorjin/utils/FileUtils.java
+++ src/io/github/curatorjin/utils/FileUtils.java
@@ -12,6 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 /**
@@ -58,6 +63,96 @@ public final class FileUtils
                 file.delete();
             }
         }
+    }
+
+    /**
+     * 获取文件特征码
+     *
+     * @param file 目标文件
+     * @param type 加密类型
+     * @return
+     * @throws IOException IO异常
+     * @throws NoSuchAlgorithmException 无加密算法异常
+     */
+    private static byte[] getFileCode(File file, String type)
+        throws IOException, NoSuchAlgorithmException
+    {
+        InputStream fis = new FileInputStream(file);
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance(type);
+        int numRead;
+
+        do
+        {
+            numRead = fis.read(buffer);
+            if (numRead > 0)
+            {
+                complete.update(buffer, 0, numRead);
+            }
+        }
+        while (numRead != -1);
+        CloseUtil.closeStream(fis);
+        return complete.digest();
+    }
+
+    /**
+     * 获取文件MD5
+     *
+     * @param file 目标文件
+     * @return
+     * @throws IOException IO异常
+     * @throws NoSuchAlgorithmException 无加密算法异常
+     */
+    public static String getMD5(File file)
+        throws IOException, NoSuchAlgorithmException
+    {
+        byte[] b = getFileCode(file, "MD5");
+        String result = "";
+        for (int i = 0; i < b.length; i++)
+        {
+            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
+    }
+
+    /**
+     * 获取文件SHA1
+     *
+     * @param file 目标文件
+     * @return
+     * @throws IOException IO异常
+     * @throws NoSuchAlgorithmException 无加密算法异常
+     */
+    public static String getSHA1(File file)
+        throws IOException, NoSuchAlgorithmException
+    {
+        byte[] b = getFileCode(file, "SHA-1");
+        String result = "";
+        for (int i = 0; i < b.length; i++)
+        {
+            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
+    }
+
+    /**
+     * 获取文件SHA256
+     *
+     * @param file 目标文件
+     * @return
+     * @throws IOException IO异常
+     * @throws NoSuchAlgorithmException 无加密算法异常
+     */
+    public static String getSHA256(File file)
+        throws IOException, NoSuchAlgorithmException
+    {
+        byte[] b = getFileCode(file, "SHA-256");
+        String result = "";
+        for (int i = 0; i < b.length; i++)
+        {
+            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
     }
 
 }
